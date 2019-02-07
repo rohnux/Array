956bd5e6e05ec04cdaba685ffc71919b825331f0
Rohan Gupta <rg.rohan26@gmail.com>
2019-02-07 21:52:36 +0530

    Hackerearth
    https://www.hackerearth.com/practice/basic-programming/implementation/basics-of-implementation/practice-problems/algorithm/pile-of-coins-d33de897/

---
+++ Piles of coins.cpp
@@ -0,0 +1,19 @@
+// Program in C++
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int p,q;
+    int t;
+    cin>>t;
+    while(t--)
+    {   cin>>p>>q;
+        if(p%2==0 || q%2==0)
+        cout<<"Ashish\n";
+        else
+        cout<<"Jeel\n";
+        
+     }
+return 0;
+}
